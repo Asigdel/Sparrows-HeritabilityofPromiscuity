be2984a424b31c3a7abe9842737476ecc3105546
IssieWinney <isabel.winney@gmail.com>
2016-01-18 11:51:30 +0000

    Add system information and useful bits

--- heritabilityPromiscuity.R
+++ heritabilityPromiscuity.R
@@ -5,6 +5,8 @@
 # Isabel Winney
 # 20160115
 
+rm(list=ls())
+
 ##############################################################################
 # loading necessary packages
 ##############################################################################
@@ -13,6 +15,36 @@ library(MCMCglmm)
 library(pedantics)
 library(RODBC)
 library(knitr)
+
+##############################################################################
+# what system, R, and package configurations am I using?
+##############################################################################
+
+#R version 3.0.2 (2013-09-25) -- "Frisbee Sailing"
+#Copyright (C) 2013 The R Foundation for Statistical Computing
+print(sessionInfo())
+
+# this is the output on 20160118
+#R version 3.0.2 (2013-09-25)
+#Platform: i386-w64-mingw32/i386 (32-bit)
+#
+#locale:
+#  [1] LC_COLLATE=English_United Kingdom.1252  LC_CTYPE=English_United Kingdom.1252   
+#[3] LC_MONETARY=English_United Kingdom.1252 LC_NUMERIC=C                           
+#[5] LC_TIME=English_United Kingdom.1252    
+#
+#attached base packages:
+#  [1] grid      stats     graphics  grDevices utils     datasets  methods   base     
+#
+#other attached packages:
+#  [1] pedantics_1.5    MasterBayes_2.52 kinship2_1.6.0   quadprog_1.5-5   genetics_1.3.8.1
+#[6] mvtnorm_1.0-2    MASS_7.3-29      gtools_3.4.2     gdata_2.13.3     combinat_0.0-8  
+#[11] MCMCglmm_2.22    ape_3.2          coda_0.17-1      Matrix_1.2-0    
+#
+#loaded via a namespace (and not attached):
+#  [1] corpcor_1.6.7   cubature_1.1-2  lattice_0.20-31 nlme_3.1-111    tensorA_0.36    tools_3.0.2    
+
+
 
 ##############################################################################
 # loading the pedigree
@@ -65,6 +97,8 @@ sqlTables(sparrowDB)
 # This means that my measure of EPO and WPO totals is only
 # based on these offspring and all the offspring that are
 # caught in the winter etc I ignore.
+
+# make some change
 
 
 ##############################################################################
